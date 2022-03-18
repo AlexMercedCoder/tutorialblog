1984877628610f6c6b36b86abff3786729d53c17
Alex Merced <alex@alexmerced.dev>
2022-03-18 13:56:20 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,35 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import { useState, useRef } from "react"
+import { render } from "react-dom"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
+  const searchRef = useRef(null)
+
+  const [renderedPosts, setRenderedPosts] = useState(posts)
+
+  const search = () => {
+    const term = searchRef.current.value
+    const results = posts.filter(({node}) => {
+      return node.frontmatter.title.toLowerCase().includes(term.toLowerCase())
+    })
+    setRenderedPosts(results)
+  }
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      {posts.map(({ node }) => {
+      <div className="search">
+      <input type="text" ref={searchRef} placeholder="search articles"/>
+      <button onClick={search}>search</button>
+      </div>
+
+      {renderedPosts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
           <article key={node.fields.slug}>
