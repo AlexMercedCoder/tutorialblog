44e65577b49344b2ff3d846bb074bd66630cebb3
Alex Merced <alex@alexmerced.dev>
2024-08-04 21:31:04 -0400

    update

--- src/components/seo.js
+++ src/components/seo.js
@@ -70,7 +70,16 @@ const SEO = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DLXYYBNDKE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DLXYYBNDKE');
+</script>
+    </Helmet>
   )
 }
 
